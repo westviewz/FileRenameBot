a356056c3332adcec19474c7bc4b45cd8ffad3bc
westviewz <nahasnj2002@gmail.com>
2022-02-17 14:15:54 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -84,7 +84,7 @@ async def start_me(bot, update):
         try:
             user = await bot.get_chat_member(update_channel, update.chat.id)
             if user.status == "kicked":
-               await update.reply_text(" Sorry,You've Been Flooding Me So My Owner Removed You From Using Me If You Think It's An Error Contact : @Faris_TG")
+               await update.reply_text(" Sorry,You've Been Flooding Me So My Owner Removed You From Using Me If You Think It's An Error Contact : @Arishem_TheJudge")
                return
         except UserNotParticipant:
             await update.reply_text(
@@ -102,12 +102,12 @@ async def start_me(bot, update):
                         InlineKeyboardButton("Help", callback_data = "ghelp")
                 ],
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Channel', url='https://t.me/Ott_streaming_updates'),
+                    InlineKeyboardButton('Channel 2', url='https://t.me/Ott_streaming_updates')
                 ],
                 [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Channel 3', url='https://t.me/Ott_streaming_updates'),
+                    InlineKeyboardButton('Channel 4', url='https://t.me/Ott_streaming_updates')
                 ]
             ]
         ),
